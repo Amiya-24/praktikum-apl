698c12bfaf39264e5ece329c51c279f0d9fb76cd
Amiya-24 <flashfazz@gmail.com>
2025-03-15 00:41:00 +0800

    salah ngartikan ternyata tadi

--- post-test/post-test-2/2409106031-MuhammadFaizLazuardi-PT-2.cpp
+++ post-test/post-test-2/2409106031-MuhammadFaizLazuardi-PT-2.cpp
@@ -1,171 +1,158 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-string jadwal[10][4];
+const int MAX_DOKTER = 10;
+string nama_dokter[MAX_DOKTER];
+string spesialis[MAX_DOKTER];
+string jadwal_mulai[MAX_DOKTER];
+string jadwal_selesai[MAX_DOKTER];
 int jumlah_dokter = 0;
 
-int main() {
-    system("cls");
-    // LOGIN
+int tampilkan_jadwal() {
+    if (jumlah_dokter == 0) {
+        cout << "Jadwal Kosong" << endl;
+        return 0;
+    }
+    for(int i = 0; i < jumlah_dokter; ++i){
+        cout << i + 1 << ". Nama: " << nama_dokter[i] << endl;
+        cout << "   " << "Spesialis: " << spesialis[i] << endl;
+        cout << "   " << "Jam Mulai: " << jadwal_mulai[i] << endl;
+        cout << "   " << "Jam Selesai: " << jadwal_selesai[i] << endl;
+        cout << " " << endl;
+    }
+    return 1;
+}
+
+bool tambah_jadwal() {
+    if (jumlah_dokter < MAX_DOKTER) {
+        cout << "Masukkan Nama Dokter: "; cin >> nama_dokter[jumlah_dokter];
+        cout << "Masukkan Spesialis: "; cin >> spesialis[jumlah_dokter];
+        cout << "Masukkan Jam Mulai: "; cin >> jadwal_mulai[jumlah_dokter];
+        cout << "Masukkan Jam Selesai: "; cin >> jadwal_selesai[jumlah_dokter];
+        jumlah_dokter++;
+        cout << "Data berhasil ditambahkan!\n";
+
+    } else {
+        cout << "Data penuh! Tidak bisa menambahkan lagi.\n";
+    }
+    return 1;
+}
+
+bool ubah_jadwal() {
+    tampilkan_jadwal();
+    int index;
+    cout << "Masukkan nomor data yang ingin diedit: ";
+    cin >> index;
+    if (index > 0 && index <= jumlah_dokter) {
+        cout << "Masukkan Nama Dokter baru: "; cin >> nama_dokter[index - 1];
+        cout << "Masukkan Spesialis baru: "; cin >> spesialis[index - 1];
+        cout << "Masukkan Jam Mulai baru: "; cin >> jadwal_mulai[index - 1];
+        cout << "Masukkan Jam Selesai baru: "; cin >> jadwal_selesai[index - 1];
+        cout << "Data berhasil diperbarui!\n";
+
+    } else {
+        cout << "Nomor data tidak valid!\n";
+    }
+    return 1;
+}
+
+bool hapus_jadwal() {
+    int index;
+    tampilkan_jadwal();
+    if(jumlah_dokter == 0) {
+        return 0;
+
+    } else {
+        cout << "Masukkan nomor data yang ingin dihapus: ";
+        cin >> index;
+        if (index > 0 && index <= jumlah_dokter) {
+            for (int i = index - 1; i < jumlah_dokter - 1; i++) {
+                nama_dokter[i] = nama_dokter[i + 1];
+                spesialis[i] = spesialis[i + 1];
+                jadwal_mulai[i] = jadwal_mulai[i + 1];
+                jadwal_selesai[i] = jadwal_selesai[i + 1];
+            }
+            jumlah_dokter--;
+            cout << "Data berhasil dihapus!\n";
+
+        } else {
+            cout << "Nomor data tidak valid!\n";
+        }
+    }
+    return 1;
+}
+
+int login(){
     int percobaan = 0;
     string username = "faiz", password = "031", login_user, login_password, pilih;
-
-    while(true){
+    while (percobaan < 3) {
         cout << "<===== Home Page =====>\n";
         cout << "1. Login\n";
         cout << "2. Keluar\n";
         cout << "Pilih Fitur: "; cin >> pilih;
-        system("cls");
 
         if(pilih == "1"){
-            while(percobaan < 3){
-                cout << "<===== Login Page =====>\n";
-                cout << "Username: "; cin >> login_user;
-                cout << "Password: "; cin >> login_password;
-                system("cls");
+            cout << "\n<===== Login Page =====>\n";
+            cout << "Username: "; cin >> login_user;
+            cout << "Password: "; cin >> login_password;
+            system("cls");
 
-                if(login_user == username and login_password == password){
-                    cout << "Login Berhasil!" << endl;
+            if (login_user == username and login_password == password) {
+                cout << "Login berhasil!\n";
+                return 0;
 
-                    // CRUD
-                    while (true){
-                        cout << "\n<===== Jadwal Praktik Dokter =====>\n";
-                        cout << "1. Tampilkan Jadwal\n";
-                        cout << "2. Tambah Jadwal\n";
-                        cout << "3. Ubah Jadwal\n";
-                        cout << "4. Hapus Jadwal\n";
-                        cout << "5. Keluar\n";
-                        cout << "Pilih Fitur: "; cin >> pilih;
-                        system("cls");
-
-                        if(pilih == "1"){
-                            // Tampilkan Jadwal
-                            system("cls");
-                            if (jumlah_dokter >= 1){
-                                for(int i = 0; i < jumlah_dokter; ++i){
-                                    cout << i + 1 << ". Nama: " << jadwal[i][0] << endl;
-                                    cout << "   " << "Spesialis: " << jadwal[i][1] << endl;
-                                    cout << "   " << "Jam Mulai: " << jadwal[i][2] << endl;
-                                    cout << "   " << "Jam Selesai: " << jadwal[i][3] << endl;
-                                    cout << " " << endl;
-                                }
-
-                            }else{
-                                cout << "Jadwal Kosong" << endl;
-                            }
-
-                        } else if(pilih == "2"){
-                            // Tambah Jadwal
-                            system("cls");
-                            if(jumlah_dokter >= 1){
-                                for(int i = 0; i <jumlah_dokter; ++i){
-                                    cout << i + 1 << ". Nama: " << jadwal[i][0] << endl;
-                                    cout << "   " << "Spesialis: " << jadwal[i][1] << endl;
-                                    cout << "   " << "Jam Mulai: " << jadwal[i][2] << endl;
-                                    cout << "   " << "Jam Selesai: " << jadwal[i][3] << endl;
-                                    cout << " " << endl;
-                                }
-
-                            }
-                            cout << "\nNama Dokter: "; cin >> jadwal[jumlah_dokter][0];
-                            cout << "Spesialis: "; cin >> jadwal[jumlah_dokter][1];
-                            cout << "Jam Mulai: "; cin >> jadwal[jumlah_dokter][2];
-                            cout << "Jam Selesai: "; cin >> jadwal[jumlah_dokter][3];
-                            jumlah_dokter++;
-                        
-                        } else if(pilih == "3"){
-                            // Ubah Jadwal
-                            system("cls");
-                            if(jumlah_dokter == 0){
-                                cout << "Jadwal Kosong" << endl;
-
-                            }else{
-                                for(int i = 0; i <jumlah_dokter; ++i){
-                                    cout << i + 1 << ". Nama: " << jadwal[i][0] << endl;
-                                    cout << "   " << "Spesialis: " << jadwal[i][1] << endl;
-                                    cout << "   " << "Jam Mulai: " << jadwal[i][2] << endl;
-                                    cout << "   " << "Jam Selesai: " << jadwal[i][3] << endl;
-                                }
-
-                                int index;
-                                cout << "Pilih ID Dokter Yang Ingin Di Ubah\n"; cin >> index;
-                                string nama, spesialis, jam_mulai, jam_selesai;
-    
-                                if(index >= 1 and index <= jumlah_dokter){
-                                    cout << "Masukan Nama Dokter: "; cin >> jadwal[index-1][0];
-                                    cout << "Masukkan Spesialis: "; cin >> jadwal[index-1][1];
-                                    cout << "Masukkan Jam Mulai: "; cin >> jadwal[index-1][2];
-                                    cout << "Masukkan Jam Selesai: "; cin >> jadwal[index-1][3];
-    
-                                }else{
-                                    cout << "ID Dokter Tidak Valid\n";
-                                }
-    
-                            }
-
-
-                        } else if(pilih == "4"){
-                            // Hapus Jadwal
-                            system("cls");
-                            if(jumlah_dokter == 0){
-                                cout << "Jadwal Kosong" << endl;
-
-                            }else{
-                                for(int i = 0; i < jumlah_dokter; ++i){
-                                    cout << i + 1 << ". Nama: " << jadwal[i][0] << endl;
-                                    cout << "   " << "Spesialis: " << jadwal[i][1] << endl;
-                                    cout << "   " << "Jam Mulai: " << jadwal[i][2] << endl;
-                                    cout << "   " << "Jam Selesai: " << jadwal[i][3] << endl;
-                                }
-                                
-                                int index;
-                                cout << "Pilih ID Dokter Yang Ingin Di Hapus"; cin >> index;
-                                if(index >= 1 and index <= jumlah_dokter){
-                                    for(int j = index-1; j < jumlah_dokter-1; ++j){
-                                        jadwal[j][0] = jadwal[j+1][0];
-                                        jadwal[j][1] = jadwal[j+1][1];
-                                        jadwal[j][2] = jadwal[j+1][2];
-                                        jadwal[j][3] = jadwal[j+1][3];
-                                    }
-
-                                    jumlah_dokter--;
-                                    cout << "Jadwal Dokter Berhasil Dihapus\n";
-
-                                } else{
-                                    cout << "ID Dokter Tidak Valid\n";
-                                }
-                            }
-
-                        } else if(pilih == "5"){
-                            system("cls");
-                            cout << "Program Di Hentikan";
-                            exit(0);
-                            
-                        } else{
-                            cout << "Pilihan Tidak Valid\n";
-                        }
-                    }
-
-
-                } else {
-                    cout << "Login Gagal! Percobaan ke-" << percobaan + 1 << "\n" << endl;
-                    percobaan++;
-                    if(percobaan == 3){
-                        cout << "Percobaan Melewati Batas\n";
-                        cout << "Program Di Hentikan";
-                        exit(0);
-                    }
+            } else {
+                cout << "Username atau password salah!\n";
+                percobaan++;
+                if (percobaan == 3) {
+                    cout << "Login gagal! Program Berhenti.\n";
+                    exit(0);
                 }
             }
-        
-        } else if(pilih == "2"){
-            cout << "Program Di Hentikan";
+        }else if(pilih == "2"){
+            cout << "Keluar dari sistem.\n";
             exit(0);
+        }else {
+            cout << "Input Yang Anda Pilih Tidak Valid. Silahkan Coba lagi.\n\n";
+        }
 
-        } else{
-            cout << "Pilihan Tidak Valid\n";
+    }
+    return 1;
+}
+
+int main() {
+    login();
+    while (true) {
+        int pilih;
+        cout << "\n<===== Jadwal Praktik Dokter =====>\n";
+        cout << "1. Tampilkan Jadwal\n";
+        cout << "2. Tambah Jadwal\n";
+        cout << "3. Ubah Jadwal\n";
+        cout << "4. Hapus Jadwal\n";
+        cout << "5. Keluar\n";
+        cout << "Pilih Fitur: "; cin >> pilih;
+        system("cls");
+
+        switch (pilih) {
+            case 1:
+                tampilkan_jadwal();
+                break;
+            case 2:
+                tambah_jadwal();
+                break;
+            case 3:
+                ubah_jadwal();
+                break;
+            case 4:
+                hapus_jadwal();
+                break;
+            case 5:
+                cout << "Terima kasih telah menggunakan program ini!\n";
+                exit(0);
+            default:
+                cout << "Pilihan tidak valid! Coba lagi.\n";
         }
     }
-
     return 0;
 }
