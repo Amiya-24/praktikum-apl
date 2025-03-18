cf3b9bf5644e1a224e9bbfe590b2dea75ebfb065
Amiya-24 <flashfazz@gmail.com>
2025-03-18 15:06:02 +0800

    Pertemuan 3

--- kelas/pertemuan-3/2409106031-MuhammadFaizLazuardi-P-3.cpp
+++ kelas/pertemuan-3/2409106031-MuhammadFaizLazuardi-P-3.cpp
@@ -43,203 +43,203 @@
 
 //                                     Contoh CRUD
 
-// #include <iostream>
-// using namespace std;
-
-// struct Alamat {
-//     string jalan;
-//     int nomor;
-//     string kota;
-// };
-
-// struct Mahasiswa {
-//     string nama;
-//     int umur;
-//     string jurusan;
-//     Alamat alamat;
-// };
-
-// #define MAX_MAHASISWA 100
-// int panjang = 0; // Jumlah elemen saat ini
-// Mahasiswa mhs[MAX_MAHASISWA]; // Array dengan ukuran tetap
-
-// int main() {
-//     system("clear");
-//     int pilihan, index;
-//     do {
-//         cout << "Menu Program" << endl;
-//         cout << "1. Tampilkan Mahasiswa" << endl;
-//         cout << "2. Tambah Mahasiswa" << endl;
-//         cout << "3. Ubah Mahasiswa" << endl;
-//         cout << "4. Hapus Mahasiswa" << endl;
-//         cout << "5. Keluar" << endl;
-//         cout << "Pilihan: "; cin >> pilihan; cout << "\n" << endl;
-//         system("clear");
-        
-//         switch (pilihan) {
-//         case 1:
-//         // Tampilkan Data Mahasiswa
-//             if (panjang == 0) {
-//                 cout << "Belum ada mahasiswa\n" << endl;
-            
-//             } else {
-//                 cout << "Daftar Mahasiswa" << endl;
-//                 cout << "==================" << endl;
-//                 for (int i = 0; i < panjang; i++) {
-//                 cout << "Mahasiswa ke-" << i + 1 << endl;
-//                 cout << "Nama: " << mhs[i].nama << endl;
-//                 cout << "Umur: " << mhs[i].umur << endl;
-//                 cout << "Jurusan: " << mhs[i].jurusan << endl;
-//                 cout << "Alamat: " << mhs[i].alamat.jalan << " No. "
-//                 << mhs[i].alamat.nomor << ", " << mhs[i].alamat.kota <<
-                
-//                 endl;
-//                 cout << endl;
-//                 }
-//             }
-//             break;
-
-//         case 2:
-//         // Tambah Data Mahasiswa
-//             if (panjang < MAX_MAHASISWA) {
-//                 cout << "Masukkan Nama Mahasiswa: ";
-//                 cin.ignore();
-//                 getline(cin, mhs[panjang].nama);
-//                 cout << "Masukkan Umur Mahasiswa: ";
-//                 cin >> mhs[panjang].umur;
-//                 cout << "Masukkan Jurusan Mahasiswa: ";
-//                 cin.ignore();
-//                 getline(cin, mhs[panjang].jurusan);
-//                 cout << "Masukkan Alamat Mahasiswa: " << endl;
-//                 cout << "Jalan: ";
-//                 getline(cin, mhs[panjang].alamat.jalan);
-//                 cout << "Nomor: ";
-//                 cin >> mhs[panjang].alamat.nomor;
-//                 cout << "Kota: ";
-//                 cin.ignore();
-//                 getline(cin, mhs[panjang].alamat.kota);
-//                 panjang++;
-//                 cout << "Mahasiswa Berhasil Ditambahkan.\n" << endl;
-
-//             } else {
-//                 cout << "Kapasitas penuh! Tidak bisa menambah mahasiswa lagi.\n" << endl;
-//             }
-//             break;
-
-//         case 3:
-//         // Ubah Data Mahasiswa
-//             if (panjang == 0) {
-//                 cout << "Belum Ada Mahasiswa Untuk Diubah.\n" << endl;
-            
-//             } else {
-//                 cout << "Daftar Mahasiswa" << endl;
-//                 cout << "==================" << endl;
-
-//                 for (int i = 0; i < panjang; i++) {
-//                     cout << "Mahasiswa ke-" << i + 1 << endl;
-//                     cout << "Nama: " << mhs[i].nama << endl;
-//                     cout << endl;
-//                 }
-//                 cout << "Masukkan Nomor Mahasiswa Yang Akan Diubah: ";
-//                 cin >> index;
-
-//                 if (index > 0 && index <= panjang) {
-//                     cout << "Masukkan Nama Mahasiswa Baru: ";
-//                     cin.ignore();
-//                     getline(cin, mhs[index - 1].nama);
-
-//                 // tambahkan untuk field lainnya
-//                 cout << "Mahasiswa Berhasil Diubah.\n" << endl;
-//                 } else {
-//                 cout << "Nomor Mahasiswa Tidak Valid.\n" << endl;
-//                 }
-//             }
-//             break;
-
-//         case 4:
-//         // Hapus Data Mahasiswa
-//             if (panjang == 0) {
-//                 cout << "Belum Ada Mahasiswa Untuk Dihapus." << endl;
-
-//             } else {
-//                 cout << "Daftar Mahasiswa" << endl;
-//                 cout << "==================" << endl;
-//                 for (int i = 0; i < panjang; i++) {
-//                     cout << "Mahasiswa ke-" << i + 1 << endl;
-//                     cout << "Nama: " << mhs[i].nama << endl;
-//                     cout << endl;
-//                 }
-
-//                 cout << "Masukkan Nomor Mahasiswa Yang Akan Dihapus: ";
-//                 cin >> index;
-//                 if (index > 0 && index <= panjang) {
-//                     for (int i = index - 1; i < panjang - 1; i++) {
-//                         mhs[i] = mhs[i + 1];
-//                     }
-//                     panjang--;
-//                     cout << "Mahasiswa Berhasil Dihapus.\n" << endl;
-//                 } else {
-//                 cout << "Nomor Mahasiswa Tidak Valid.\n" << endl;
-//                 }
-//             }
-//             break;
-
-//         case 5:
-//             cout << "Program selesai." << endl;
-//             break;
-
-//         default:
-//             cout << "Pilihan tidak valid.\n" << endl;
-//             break;
-//         }
-//     } while (pilihan != 5);
-// return 0;
-// }
-
-//                                      Latihan
-
 #include <iostream>
 using namespace std;
 
-struct tanggal {
-    int hari;
-    string bulan;
-    int tahun;
+struct Alamat {
+    string jalan;
+    int nomor;
+    string kota;
 };
 
-struct Mahasiswa{
+struct Mahasiswa {
     string nama;
-    string nim;
-    string kelas;
-    tanggal tanggal_lhr;
+    int umur;
+    string jurusan;
+    Alamat alamat;
 };
 
-int main(){
-    Mahasiswa mhs_baru;
-    Mahasiswa mhs_lama;
+#define MAX_MAHASISWA 100
+int panjang = 0; // Jumlah elemen saat ini
+Mahasiswa mhs[MAX_MAHASISWA]; // Array dengan ukuran tetap
 
-    mhs_baru.nama = "Faiz";
-    mhs_baru.nim = "031";
-    mhs_baru.kelas = "A2'24";
-    mhs_baru.tanggal_lhr.hari = 24;
-    mhs_baru.tanggal_lhr.bulan = "08";
-    mhs_baru.tanggal_lhr.tahun = 2000;
+int main() {
+    system("clear");
+    int pilihan, index;
+    do {
+        cout << "Menu Program" << endl;
+        cout << "1. Tampilkan Mahasiswa" << endl;
+        cout << "2. Tambah Mahasiswa" << endl;
+        cout << "3. Ubah Mahasiswa" << endl;
+        cout << "4. Hapus Mahasiswa" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "Pilihan: "; cin >> pilihan; cout << "\n" << endl;
+        system("clear");
+        
+        switch (pilihan) {
+        case 1:
+        // Tampilkan Data Mahasiswa
+            if (panjang == 0) {
+                cout << "Belum ada mahasiswa\n" << endl;
+            
+            } else {
+                cout << "Daftar Mahasiswa" << endl;
+                cout << "==================" << endl;
+                for (int i = 0; i < panjang; i++) {
+                cout << "Mahasiswa ke-" << i + 1 << endl;
+                cout << "Nama: " << mhs[i].nama << endl;
+                cout << "Umur: " << mhs[i].umur << endl;
+                cout << "Jurusan: " << mhs[i].jurusan << endl;
+                cout << "Alamat: " << mhs[i].alamat.jalan << " No. "
+                << mhs[i].alamat.nomor << ", " << mhs[i].alamat.kota <<
+                
+                endl;
+                cout << endl;
+                }
+            }
+            break;
 
-    // cout << "Nama : " << mhs_baru.nama << endl;
-    // cout << "Nim : " << mhs_baru.nim << endl;
-    // cout << "Kelas : " << mhs_baru.kelas << endl;
-    // cout << "Tanggal Lahir : " << mhs_baru.tanggal_lhr.hari << "/" << mhs_baru.tanggal_lhr.bulan << "/" << mhs_baru.tanggal_lhr.tahun << "\n" << endl;
-    
-    Mahasiswa mhs_abadi[5];
-    mhs_abadi[0].nama = "faiz";
-    mhs_abadi[1].nama = "zeydan";
-    mhs_abadi[2].nama = "fathur";
-    mhs_abadi[3].nama = "amin";
-    mhs_abadi[4].nama = "zaky";
+        case 2:
+        // Tambah Data Mahasiswa
+            if (panjang < MAX_MAHASISWA) {
+                cout << "Masukkan Nama Mahasiswa: ";
+                cin.ignore();
+                getline(cin, mhs[panjang].nama);
+                cout << "Masukkan Umur Mahasiswa: ";
+                cin >> mhs[panjang].umur;
+                cout << "Masukkan Jurusan Mahasiswa: ";
+                cin.ignore();
+                getline(cin, mhs[panjang].jurusan);
+                cout << "Masukkan Alamat Mahasiswa: " << endl;
+                cout << "Jalan: ";
+                getline(cin, mhs[panjang].alamat.jalan);
+                cout << "Nomor: ";
+                cin >> mhs[panjang].alamat.nomor;
+                cout << "Kota: ";
+                cin.ignore();
+                getline(cin, mhs[panjang].alamat.kota);
+                panjang++;
+                cout << "Mahasiswa Berhasil Ditambahkan.\n" << endl;
 
-    for(int i = 0; i < 5; i++){
-        cout << mhs_abadi[i].nama << endl;
-    }
+            } else {
+                cout << "Kapasitas penuh! Tidak bisa menambah mahasiswa lagi.\n" << endl;
+            }
+            break;
 
-    return 0;
+        case 3:
+        // Ubah Data Mahasiswa
+            if (panjang == 0) {
+                cout << "Belum Ada Mahasiswa Untuk Diubah.\n" << endl;
+            
+            } else {
+                cout << "Daftar Mahasiswa" << endl;
+                cout << "==================" << endl;
+
+                for (int i = 0; i < panjang; i++) {
+                    cout << "Mahasiswa ke-" << i + 1 << endl;
+                    cout << "Nama: " << mhs[i].nama << endl;
+                    cout << endl;
+                }
+                cout << "Masukkan Nomor Mahasiswa Yang Akan Diubah: ";
+                cin >> index;
+
+                if (index > 0 && index <= panjang) {
+                    cout << "Masukkan Nama Mahasiswa Baru: ";
+                    cin.ignore();
+                    getline(cin, mhs[index - 1].nama);
+
+                // tambahkan untuk field lainnya
+                cout << "Mahasiswa Berhasil Diubah.\n" << endl;
+                } else {
+                cout << "Nomor Mahasiswa Tidak Valid.\n" << endl;
+                }
+            }
+            break;
+
+        case 4:
+        // Hapus Data Mahasiswa
+            if (panjang == 0) {
+                cout << "Belum Ada Mahasiswa Untuk Dihapus." << endl;
+
+            } else {
+                cout << "Daftar Mahasiswa" << endl;
+                cout << "==================" << endl;
+                for (int i = 0; i < panjang; i++) {
+                    cout << "Mahasiswa ke-" << i + 1 << endl;
+                    cout << "Nama: " << mhs[i].nama << endl;
+                    cout << endl;
+                }
+
+                cout << "Masukkan Nomor Mahasiswa Yang Akan Dihapus: ";
+                cin >> index;
+                if (index > 0 && index <= panjang) {
+                    for (int i = index - 1; i < panjang - 1; i++) {
+                        mhs[i] = mhs[i + 1];
+                    }
+                    panjang--;
+                    cout << "Mahasiswa Berhasil Dihapus.\n" << endl;
+                } else {
+                cout << "Nomor Mahasiswa Tidak Valid.\n" << endl;
+                }
+            }
+            break;
+
+        case 5:
+            cout << "Program selesai." << endl;
+            break;
+
+        default:
+            cout << "Pilihan tidak valid.\n" << endl;
+            break;
+        }
+    } while (pilihan != 5);
+return 0;
 }
+
+// //                                      Latihan
+
+// #include <iostream>
+// using namespace std;
+
+// struct tanggal {
+//     int hari;
+//     string bulan;
+//     int tahun;
+// };
+
+// struct Mahasiswa{
+//     string nama;
+//     string nim;
+//     string kelas;
+//     tanggal tanggal_lhr;
+// };
+
+// int main(){
+//     Mahasiswa mhs_baru;
+//     Mahasiswa mhs_lama;
+
+//     mhs_baru.nama = "Faiz";
+//     mhs_baru.nim = "031";
+//     mhs_baru.kelas = "A2'24";
+//     mhs_baru.tanggal_lhr.hari = 24;
+//     mhs_baru.tanggal_lhr.bulan = "08";
+//     mhs_baru.tanggal_lhr.tahun = 2000;
+
+//     cout << "Nama : " << mhs_baru.nama << endl;
+//     cout << "Nim : " << mhs_baru.nim << endl;
+//     cout << "Kelas : " << mhs_baru.kelas << endl;
+//     cout << "Tanggal Lahir : " << mhs_baru.tanggal_lhr.hari << "/" << mhs_baru.tanggal_lhr.bulan << "/" << mhs_baru.tanggal_lhr.tahun << "\n" << endl;
+    
+//     Mahasiswa mhs_abadi[5];
+//     mhs_abadi[0].nama = "faiz";
+//     mhs_abadi[1].nama = "zeydan";
+//     mhs_abadi[2].nama = "fathur";
+//     mhs_abadi[3].nama = "amin";
+//     mhs_abadi[4].nama = "zaky";
+
+//     for(int i = 0; i < 5; i++){
+//         cout << mhs_abadi[i].nama << endl;
+//     }
+
+//     return 0;
+// }
